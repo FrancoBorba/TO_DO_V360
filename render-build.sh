14c481f9a003183ba46a291898e428f11b46abd0
FrancoBorba <franco.borba14@gmail.com>
2025-05-17 17:44:29 -0300

    Configura scripts npm para deploy

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -o errexit
 
+# Instala dependências
 bundle install
+npm install
+
+# Build dos assets
+npm run build:css
 rails assets:precompile
+
+# Migrações
 rails db:migrate
