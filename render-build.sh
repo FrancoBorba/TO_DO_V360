55fac406bc3c22048b3bc62ca581057d0e0acd80
FrancoBorba <franco.borba14@gmail.com>
2025-05-17 19:37:11 -0300

    Preparação final para deploy: migração usuario_id

--- render-build.sh
+++ render-build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -o errexit
 
-# Configuração do ambiente
-export RAILS_ENV=production
-export BUNDLE_PATH=vendor/bundle
-
 # Instala dependências
 bundle install
 
-# Recria o banco e aplica TODAS as migrações
-bundle exec rails db:drop db:create db:migrate
+# Executa migrações do banco de dados
+bundle exec rails db:migrate
 
-# Compila assets
+# Compila assets para produção
 bundle exec rails assets:precompile
