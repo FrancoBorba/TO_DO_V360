acc6eb00d8367e8d40a4028fb79bc23c2b4add0e
FrancoBorba <franco.borba14@gmail.com>
2025-05-17 19:01:06 -0300

    Recria banco de dados no Render

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -o errexit
 
-# 1. Instala dependências
-bundle install
+# 1. Encerra conexões existentes
+rails db -e production <<EOF
+  UPDATE pg_database SET datallowconn = 'false' WHERE datname = 'todo_v360_vf6j';
+  SELECT pg_terminate_backend(pg_stat_activity.pid)
+  FROM pg_stat_activity
+  WHERE pg_stat_activity.datname = 'todo_v360_vf6j';
+EOF
 
-# 2. Recria o banco com TODAS as migrações (incluindo a criação da tabela lists)
+# 2. Recria o banco
 rails db:drop db:create db:migrate
 
 # 3. Compila assets
