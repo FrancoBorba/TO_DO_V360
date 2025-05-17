53f656ec382bb8fa848aeb58835026b3c8473a65
FrancoBorba <franco.borba14@gmail.com>
2025-05-17 18:04:59 -0300

    Corrige migrações para o Render

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -o errexit
 
+# Instala dependências
 bundle install
-rails tailwindcss:build
+
+# Garante que as migrações rodem
+rails db:migrate || rails db:create db:migrate
+
+# Compila assets
 rails assets:precompile
-rails db:migrate
